2bc8b95605c487f45d5b72598640f3b31a12d17b
Zach Drago <zachdrago@MacBook-Pro-3.local>
2015-12-22 05:25:16 -0500

    remove mouse snow

--- js/advent.js
+++ js/advent.js
@@ -18,7 +18,7 @@ var vid = document.getElementById("mp3");
       var self = $(this);
       var covs = self.find(".cover");
       if (day == 22) {
-        $('<script type="text/javascript" src="js/mouseSnow.js"></script>').appendTo('head');
+        // $('<script type="text/javascript" src="js/mouseSnow.js"></script>').appendTo('head');
         $('<div class="all"></div>').appendTo('body');
       }
     });

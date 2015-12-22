650a2ffb12fb50aabd7d69dea787800eefb1ebd2
Zach Drago <zachdrago@MacBook-Pro-3.local>
2015-12-22 05:17:49 -0500

    date test

--- js/advent.js
+++ js/advent.js
@@ -17,7 +17,7 @@ var vid = document.getElementById("mp3");
       vid.volume = 1.00;
       var self = $(this);
       var covs = self.find(".cover");
-      if (day == 25) {
+      if (day == 22) {
         $('<div class="all"></div>').appendTo('body');
         $('<script type="text/javascript" src="js/mouseSnow.js"></script>').appendTo('body');
       }
@@ -46,7 +46,7 @@ var dayNames = ["Sunday",
               "Saturday"];
 
 var newDate = new Date();
-newDate.setDate(newDate.getDate());    
+newDate.setDate(newDate.getDate() +6);    
 $('#date').html(dayNames[newDate.getDay()] + " " + monthNames[newDate.getMonth()] + ' ' + newDate.getDate() + ', ' + newDate.getFullYear());
 
 var day = newDate.getDate();
